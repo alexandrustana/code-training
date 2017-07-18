dc487464d144614d46c2e71e362bad8be9ac5b49
Alex <Alex>
2017-07-18 17:05:48 +0300

    Finished 231 A

--- Algorithms/src/com/codeforce/problemset/A231/Source.java
+++ Algorithms/src/com/codeforce/problemset/A231/Source.java
@@ -1,7 +1,27 @@
 package com.codeforce.problemset.A231;
 
+import java.util.Scanner;
+
 public final class Source {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
+		int n = scan.nextInt();
+
+		int number = solvableProblems(n, scan);
+
+		System.out.println(number);
+	}
+
+	private static int solvableProblems(int n, Scanner scan) {
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			int temp = scan.nextInt() + scan.nextInt() + scan.nextInt();
+			if (temp >= 2) {
+				result++;
+			}
+		}
+
+		return result;
 	}
 }
