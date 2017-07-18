3e876140a1e221d1d6fe337f674e7dfe2053300b
Alex <alexandrustana2@gmail.com>
2017-07-19 00:12:39 +0300

    Finished 339 A

--- Algorithms/src/com/codeforce/problemset/A339/Source.java
+++ Algorithms/src/com/codeforce/problemset/A339/Source.java
@@ -1,7 +1,36 @@
 package com.codeforce.problemset.A339;
 
+import java.util.Scanner;
+
 public final class Source {
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+
+		String calculus = scan.next();
+		printOrderedCalculus(calculus);
+	}
+
+	private static void printOrderedCalculus(String calculus) {
+		calculus = calculus.replaceAll("\\+", "");
+		int[] numbers = getCalculusNumbers(calculus);
+		printCalculus(numbers);
+	}
+
+	private static void printCalculus(int[] numbers) {
+		String result = "";
+		for(int i = 0 ; i < numbers.length ; i++) {
+			for(int j = 0 ; j < numbers[i]; j++) {
+				result += (i + 1) + "+";
+			}
+		}
+		System.out.println(result.substring(0, result.length() - 1));
+	}
+
+	private static int[] getCalculusNumbers(String calculus) {
+		int[] freq = { 0, 0, 0 };
+		for(int i = 0 ; i < calculus.length() ; i++) {
+			freq[(calculus.charAt(i) - '0')  - 1]++;
+		}
+		return freq;
 	}
 }
