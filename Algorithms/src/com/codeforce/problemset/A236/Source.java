8469c7ebf9e9b1b9a322ed5ebc9e53c635078fca
Alex <Alex>
2017-07-19 22:25:22 +0300

    Finished 236 A

--- Algorithms/src/com/codeforce/problemset/A236/Source.java
+++ Algorithms/src/com/codeforce/problemset/A236/Source.java
@@ -1,14 +1,17 @@
 package com.codeforce.problemset.A236;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public final class Source {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String name = scan.next();
-		int[] freq = new int[26];
-		for (int i = 0; i < name.length(); i++) {
-			
-		}
-	}
+     public static void main(String[] args){
+         Scanner scan=new Scanner(System.in);
+         String username = scan.next();
+         int[] freq = new int[26];
+         for(int i=0;i<username.length();i++){
+             freq[username.charAt(i) - 'a'] =  1;
+         }
+         int gender = IntStream.of(freq).sum();
+         System.out.println(gender %2==0? "CHAT WITH HER!": "IGNORE HIM!");
+     }
 }
