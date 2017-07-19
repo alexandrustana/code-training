b2f2ebe31010fa8f8a4e001f9fbfa681b65c682a
Alex <Alex>
2017-07-19 17:02:03 +0300

    WIP 236 A

---
+++ Algorithms/src/com/codeforce/problemset/A236/Source.java
@@ -0,0 +1,12 @@
+package com.codeforce.problemset.A236;
+
+import java.util.Scanner;
+
+public final class Source {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String name = scan.next();
+		int[] freq = new int[26];
+		
+	}
+}
