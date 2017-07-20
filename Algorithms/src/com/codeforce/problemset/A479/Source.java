efac81715e09ffd5aea69881bbf7bb6badd7548e
Alex <Alex>
2017-07-20 18:06:01 +0300

    WIP 479 A

---
+++ Algorithms/src/com/codeforce/problemset/A479/Source.java
@@ -0,0 +1,17 @@
+package com.codeforce.problemset.A479;
+
+import java.util.Scanner;
+
+public final class Source {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+
+		int res = 0;
+
+		int[] op = { 0, 0, 0, 1, 1, 0, 1, 1 };
+	}
+}
