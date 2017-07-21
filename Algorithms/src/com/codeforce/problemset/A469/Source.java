cddd36a933b9e5fa1cdcad616b802f03355605ee
Alex <Alex>
2017-07-21 11:49:03 +0300

    Finished 469 A

--- Algorithms/src/com/codeforce/problemset/A469/Source.java
+++ Algorithms/src/com/codeforce/problemset/A469/Source.java
@@ -5,29 +5,29 @@ import java.util.Scanner;
 public final class Source {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		int levels = scan.nextInt();
 		int[] game = new int[levels];
-		
+
 		fillPlayer(scan, game);
 		fillPlayer(scan, game);
-		
-		System.out.println(canPassAllLevels(game) ? "I become the guy." : "Oh, my keyboard!");
+
+		System.out.println(canPassAllLevels(game, levels) ? "I become the guy." : "Oh, my keyboard!");
 	}
 
-	private static boolean canPassAllLevels(int[] game) {
+	private static boolean canPassAllLevels(int[] game, int levels) {
 		int n = 0;
-		for(int i = 0 ; i < game.length; i++) {
-			if(game[i] == 1) {
+		for (int i = 0; i < levels; i++) {
+			if (game[i] == 1) {
 				n++;
 			}
 		}
-		return n == game.length - 1;
+		return n == game.length;
 	}
 
 	private static void fillPlayer(Scanner scan, int[] game) {
 		int p = scan.nextInt();
-		for(int i = 0 ; i < game.length ; i++) {
+		for (int i = 0; i < p; i++) {
 			game[scan.nextInt() - 1] = 1;
 		}
 	}
