bd6341aa7e6b4a074b1395f5828352baa2a158e9
Alex <alexandrustana2@gmail.com>
2017-07-22 13:40:53 +0300

    Finished 460 A

---
+++ Algorithms/src/com/codeforce/problemset/A460/Source.java
@@ -0,0 +1,16 @@
+package com.codeforce.problemset.A460;
+
+import java.util.Scanner;
+
+public class Source {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+
+		System.out.println(n +  (n  - 1) / (m - 1));
+	}
+
+}
