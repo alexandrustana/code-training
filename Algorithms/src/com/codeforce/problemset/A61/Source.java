84b345fac874ca506617145a42f91ea9b252d845
Alex <alexandrustana2@gmail.com>
2017-07-22 13:25:03 +0300

    Finished 16 A

---
+++ Algorithms/src/com/codeforce/problemset/A61/Source.java
@@ -0,0 +1,24 @@
+package com.codeforce.problemset.A61;
+
+import java.util.Scanner;
+
+public class Source {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String number1 = scan.next();
+		String number2 = scan.next();
+		
+		String result = "";
+		for(int i = 0 ; i < number1.length() ; i++) {
+			result +=( (getByte(number1.charAt(i)) ^ getByte(number2.charAt(i))));
+		}
+		
+		System.out.println(result);
+	}
+	
+	public static byte getByte(char code) {
+		return (byte) (code - '0');
+	}
+
+}
