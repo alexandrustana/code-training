7f6a23da530bd47495addf27913d4b37c2eb1442
Alex <Alex>
2017-07-26 17:47:55 +0300

    WIP  4 C

---
+++ Algorithms/src/com/codeforce/problemset/C4/Source.java
@@ -0,0 +1,27 @@
+package com.codeforce.problemset.C4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Source {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		List<String> users = new ArrayList<>();
+		String temp = "";
+		
+		for(int i = 0 ; i <= n ; i++) {
+			temp = scan.nextLine();
+			if(!users.contains(temp)) {
+				users.add(temp);
+				System.out.println("OK");
+			}else {
+				System.out.println(temp + "1");
+			}
+		}
+	}
+
+}
