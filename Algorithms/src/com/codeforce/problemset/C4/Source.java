9346b8f5f5a7a0dcfcdd7d604f10d4147c11542e
Alex <Alex>
2017-07-27 10:25:45 +0300

    Finished  4 C

--- Algorithms/src/com/codeforce/problemset/C4/Source.java
+++ Algorithms/src/com/codeforce/problemset/C4/Source.java
@@ -1,25 +1,27 @@
 package com.codeforce.problemset.C4;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Source {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		int n = scan.nextInt();
-		List<String> users = new ArrayList<>();
+		Map<String, Integer> users = new HashMap<>();
 		String temp = "";
-		
-		for(int i = 0 ; i < n ; i++) {
+
+		for (int i = 0; i < n; i++) {
 			temp = scan.next();
-			if(!users.contains(temp)) {
-				users.add(temp);
+			if (!users.keySet().contains(temp)) {
+				users.put(temp, 1);
 				System.out.println("OK");
-			}else {
-				System.out.println(temp + "1");
+			} else {
+				int times = users.get(temp);
+				users.put(temp, times + 1);
+				System.out.println(temp + times);
 			}
 		}
 	}
