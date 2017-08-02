324772626c1eab4ece22860445754e2c43a34505
Alex <Alex>
2017-08-02 15:59:00 +0300

    added randomized quicksort

--- Algorithms/src/com/algorithms/sorting/quick/Source.java
+++ Algorithms/src/com/algorithms/sorting/quick/Source.java
@@ -1,15 +1,36 @@
 package com.algorithms.sorting.quick;
 
+import java.util.Random;
+
 public class Source {
 
 	public static void main(String[] args) {
-		int[] a = { 2, 8, 7, 1, 3, 5, 6, 4 };
+		// int[] a = { 2, 8, 7, 1, 3, 5, 6, 4 };
+		//
+		// sort(a, 0, a.length - 1);
 
-		sort(a, 0, a.length - 1);
-
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
+		// for (int i = 0; i < a.length; i++) {
+		// System.out.println(a[i]);
+		// }
+		
+		Random random = new Random();
+		int SIZE = 100000000;
+		int[] a = new int[SIZE];
+		for (int i = 0; i < SIZE; i++) {
+			a[i] = random.nextInt();
 		}
+
+		long start = System.currentTimeMillis();
+		sort(a, 0, SIZE - 1);
+		System.out.println(System.currentTimeMillis() - start);
+
+	}
+
+	public static int randomizedPartition(int[] a, int l, int r) {
+		Random random = new Random();
+		int i = random.nextInt(r - l) + l;
+		swap(i, r, a);
+		return partition(a, l, r);
 	}
 
 	public static void sort(int[] a, int l, int r) {
