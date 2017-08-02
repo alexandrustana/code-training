f22788e4250d0673b23bd8a71cc33bef61648396
Alex <Alex>
2017-08-02 15:39:22 +0300

    added quicksort

---
+++ Algorithms/src/com/algorithms/sorting/quick/Source.java
@@ -0,0 +1,42 @@
+package com.algorithms.sorting.quick;
+
+public class Source {
+
+	public static void main(String[] args) {
+		int[] a = { 2, 8, 7, 1, 3, 5, 6, 4 };
+
+		sort(a, 0, a.length - 1);
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+	public static void sort(int[] a, int l, int r) {
+		if (l < r) {
+			int p = partition(a, l, r);
+			sort(a, l, p - 1);
+			sort(a, p + 1, r);
+		}
+	}
+
+	private static int partition(int[] a, int l, int r) {
+		int x = a[r];
+		int i = l - 1;
+		for (int j = l; j < r; j++) {
+			if (a[j] <= x) {
+				i++;
+				swap(i, j, a);
+			}
+		}
+		swap(i + 1, r, a);
+		return i + 1;
+	}
+
+	private static void swap(int i, int j, int[] a) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+}
