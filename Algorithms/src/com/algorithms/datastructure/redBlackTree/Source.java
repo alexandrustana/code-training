3fcdc1d7f89ac50fa4450240482455c4570b1b36
Alex <alexandrustana2@gmail.com>
2017-08-11 16:22:55 +0300

    WIP add rbTree

--- Algorithms/src/com/algorithms/datastructure/redBlackTree/Source.java
+++ Algorithms/src/com/algorithms/datastructure/redBlackTree/Source.java
@@ -8,56 +8,116 @@ public class Source {
 
 }
 
-class Tree {
-	public Node root;
+class RBTree {
+	Node root = null;
+	int BLACK = 0;
+	int RED = 1;
 
-	public void leftRotate(Node x) {
+	public void rotateLeft(Node x) {
 		Node y = x.right;
 		x.right = y.left;
-
 		if (y.left != null) {
-			y.left.parent = x;
+			y.left.p = x;
 		}
-
-		y.parent = x.parent;
-		if (x.parent == null) {
+		y.p = x.p;
+		if (x.p == null) {
 			root = y;
-		} else if (x.parent.left == x) {
-			x.parent.left = y;
+		} else if (x.p.left == x) {
+			x.p.left = y;
 		} else {
-			x.parent.right = y;
+			x.p.right = y;
 		}
-
 		y.left = x;
-		x.parent = y;
+		x.p = y;
 	}
 
-	public void rotateRight(Node x) {
-		Node y = x.left;
-		x.left = y.right;
-
-		if (x.left != null) {
-			x.left.parent = y;
+	public void rotateRight(Node y) {
+		Node x = y.left;
+		y.left = x.right;
+		if (x.right != null) {
+			x.right.p = y;
 		}
-
-		x.parent = y.parent;
-		if (x.parent == null) {
-			root = y;
-		} else if (x.parent.left == x) {
-			x.parent.left = y;
+		x.p = y.p;
+		if (y.p == null) {
+			root = x;
+		} else if (y.p.left == y) {
+			y.p.left = x;
 		} else {
-			x.parent.right = y;
+			y.p.right = x;
+		}
+		x.right = y;
+		y.p = x;
+	}
+
+	public void rbInsert(Node n) {
+		Node x = root;
+		Node y = null;
+		while (x != null) {
+			y = x;
+			if (n.key < x.key) {
+				x = x.left;
+			} else {
+				x = x.right;
+			}
 		}
 
-		y.right = x;
-		x.parent = y;
+		n.p = y;
+		if (y == null) {
+			root = n;
+		}
+		if (n.key < y.key) {
+			y.left = n;
+		} else {
+			y.right = n;
+		}
+		n.color = RED;
+		rbInsertFixup(n);
+	}
+
+	public void rbInsertFixup(Node n) {
+		while (n.p.color == RED) {
+			if (n.p == n.p.p.left) {
+				Node y = n.p.p.right;
+				if (y.color == RED) {
+					n.p.color = BLACK;
+					y.color = BLACK;
+					n.p.p.color = RED;
+					n = n.p.p;
+				} else {
+					if (n == n.p.right) {
+						n = n.p;
+						rotateLeft(n);
+					}
+					n.p.color = BLACK;
+					n.p.p.color = RED;
+					rotateRight(n.p.p);
+				}
+			} else {
+				Node y = n.p.p.left;
+				if (y.color == RED) {
+					n.p.color = BLACK;
+					y.color = BLACK;
+					n.p.p.color = RED;
+					n = n.p.p;
+				} else {
+					if (n == n.p.right) {
+						n = n.p;
+						rotateLeft(n);
+					}
+					n.p.color = BLACK;
+					n.p.p.color = RED;
+					rotateRight(n.p.p);
+				}
+			}
+		}
+		root.color = BLACK;
 	}
 }
 
 class Node {
-	public Node left;
-	public Node right;
-	public Node parent;
-	public int color;
-	public int key;
+	Node left;
+	Node right;
+	Node p;
+	int color;
+	int key;
 }
