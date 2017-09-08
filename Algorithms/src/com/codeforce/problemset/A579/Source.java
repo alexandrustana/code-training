036b9b00c4981fe1a5a5eb2fc47f772ce4379568
Alex <alexandrustana2@gmail.com>
2017-09-08 22:34:22 +0300

    Finished 579 A

---
+++ Algorithms/src/com/codeforce/problemset/A579/Source.java
@@ -0,0 +1,13 @@
+package com.codeforce.problemset.A579;
+
+import java.util.Scanner;
+
+public class Source {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		System.out.println(Integer.bitCount(n));
+	}
+
+}
